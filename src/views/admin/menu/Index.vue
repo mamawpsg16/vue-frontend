6bc457e2be120e508b99c9ce5bf409e0bc1c101a
Kevin Mensah <48464499+mamawpsg16@users.noreply.github.com>
2024-07-02 10:02:38 +0800

    feat(menu module)
    - Adding menu feature (Ongoing)

---
+++ src/views/admin/menu/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <div class="col-6">
+      <NestedDraggable v-if="list.length" :list="list"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import NestedDraggable from './components/NestedDraggable.vue';
+  export default {
+    components:{
+      NestedDraggable
+    },
+    data(){
+      return{
+        list: [
+          { id: 1, name: 'Parent 1', children: [{ id: 11, name: 'Child 1-1', children:[] }, { id: 12, name: 'Child 1-2', children:[] }] },
+          { id: 2, name: 'Parent 2', children: [{ id: 21, name: 'Child 2-1', children:[] }, { id: 22, name: 'Child 2-2', children:[] }] },
+        ],
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/admin/menu/components/NestedDraggable.vue
@@ -0,0 +1,59 @@
+<template>
+  <draggable class="dragArea" tag="ul" :list="list" :group="{ name: 'g1' }">
+    <li v-for="el in list" :key="el.name">
+      <p class="item-name">{{ el.name }}</p>
+      <nested-draggable :list="el.children" />
+    </li>
+  </draggable>
+</template>
+
+<script>
+import { VueDraggableNext } from 'vue-draggable-next'
+export default {
+  name: 'nested-draggable',
+  components: {
+    draggable: VueDraggableNext,
+  },
+  props: {
+    list: {
+      required: true,
+      type: Array,
+      default: []
+    }
+  },
+}
+</script>
+<style scoped>
+ul,
+li {
+  text-decoration: none;
+  list-style-type: none;
+}
+
+.dragArea {
+  /* min-height: 30px; */
+  /* border: 1px solid black; */
+  /* border-radius: 5px; */
+  margin-top: 10px;
+  /* color: var(--cui-list-group-color); */
+}
+
+.item-name {
+  font-weight: bold;
+  color: var(--cui-list-group-color);
+  text-decoration: none;
+  /* Remove text decoration */
+  border: 1px solid black;
+  margin-top: 10px;
+  padding: 10px;
+}
+
+.drag-item:hover {
+  background-color: #f1f1f1;
+}
+
+.nested-list {
+  margin-top: 10px;
+  padding-left: 20px;
+}
+</style>
